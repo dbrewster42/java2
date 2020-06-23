79e8a4e1d0856f28ee34ba74712e1b02451c98d1
Devon Brewster <devonbrewster42@gmail.com>
2020-06-22 20:05:09 -0500

    First round

--- exercise2/Arrays.java
+++ exercise2/Arrays.java
@@ -1,19 +1,45 @@
-package exercise2;
-
 class Arrays {
-    //Take the arrays.js file, and refactor the code into Java in this file.
-    int[] nums;
-    String[] greetings;
-    boolean[] binaries;
+    // Take the arrays.js file, and refactor the code into Java in this file.
+    static int[] nums = { 10, 23, 3, 4, 5, 2, 1 };
+    static String[] greetings = { "hello", "howdy", "hallo", "hola", "bonjour", "ciao", "أهلا" };
+    static boolean[] binaries = { false, true, true, true, false, false, false, true, true, true, true, true, false,
+            true, false, false, true, false };
 
-    void printNums() {};
-    void printGreetings() {};
-    void printBinaries() {};
+    public static void printNums(int[] arr) {
+        // String arrString = Arrays.toString(arr);
+        // System.out.println(arrString);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printGreetings(String[] arr) {
+        for (String i : arr) {
+            System.out.println(i);
+        }
+        // String arString = Arrays.toString(arr);
+        // System.out.println(arString);
+    }
+
+    public static void printBinaries(boolean[] arr) {
+        for (boolean i : arr) {
+            System.out.println(i);
+        }
+    };
+
+    public static void main(String[] args) {
+        printGreetings(greetings);
+        printNums(nums);
+        printBinaries(binaries);
+    }
+
 }
+// const nums = [10,23,3,4,5,2,1];
+// const greetings = ["hello","howdy","hallo","hola","bonjour","ciao","أهلا"];
+// const binaries = [0,1,1,1,0,0,0,1,1,1,1,1,0,1,0,0,1,0];
 
+// nums.forEach(n => console.log(n));
 
+// greetings.forEach(g => console.log(g));
 
-
-
-
-
+// binaries.forEach(b => console.log(!!b));
--- udemy_exercises/bank/Branch.java
+++ udemy_exercises/bank/Branch.java
@@ -1,11 +1,47 @@
-package bank;
+import java.util.ArrayList;
 
 public class Branch {
+     private String name;
+     private ArrayList<Customer> customers;
+
+     public Branch(String name) {
+          this.name = name;
+          this.customers = new ArrayList<Customer>();
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public boolean newCustomer(String customerName, double initialAmount) {
+          if (findCustomer(customerName) == null) {
+               this.customers.add(new Customer(customerName, initialAmount));
+               return true;
+          }
+          return false;
+     }
+
+     public boolean addCustomerTransaction(String customerName, double amount) {
+          Customer existingCustomer = findCustomer(customerName);
+          if (existingCustomer != null) {
+               existingCustomer.addTransaction(amount);
+               return true;
+          }
+          return false;
+     }
+
+     private Customer findCustomer(String name) {
+          for (int i = 0; i < this.customers.size(); i++) {
+               Customer customer = this.customers.get(i);
+               if (customer.getName().equals(name)) {
+                    return customer;
+               }
+          }
+          return null;
+     }
 }
 
 /*
-**Branch Class**:
-     - addCustomer and initial transaction amount.
-     - Also needs to add additional transactions for that customer/branch
-*/
-
+ ** Branch Class**: - addCustomer and initial transaction amount. - Also needs to
+ * add additional transactions for that customer/branch
+ */
