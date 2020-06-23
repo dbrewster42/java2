3196e66250b8df09a370de3ebcb8c4aa243fff64
Devon Brewster <devonbrewster42@gmail.com>
2020-06-23 13:47:43 -0500

    Redid 2nd assignment

--- udemy_exercises/bank/Bank.java
+++ udemy_exercises/bank/Bank.java
@@ -2,13 +2,62 @@ import java.util.ArrayList;
 
 public class Bank {
   private ArrayList<Branch> branches;
+  private String name;
 
-  public void setBranch() {
-
+  public Bank(String name) {
+    this.name = name;
+    this.branches = new ArrayList<Branch>();
   }
 
-  public static void main(String[] args) {
+  public void addBranch(String branchName) {
+    this.branches.add(new Branch(branchName));
+  }
 
+  public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+    Branch branch = findBranch(branchName);
+    if (branch != null) {
+      return branch.newCustomer(customerName, initialAmount);
+    }
+    return false;
+  }
+
+  public boolean addCustomerTrans(String branchName, String customerName, double amount) {
+    Branch branch = findBranch(branchName);
+    if (branch != null) {
+      return branch.addCustomerTransaction(customerName, amount);
+    }
+    return false;
+  }
+
+  private Branch findBranch(String name) {
+    for (int i = 0; i < this.branches.size(); i++) {
+      Branch branch = this.branches.get(i);
+      if (branch.getName().equals(name)) {
+        return branch;
+      }
+    }
+    return null;
+  }
+
+  public boolean listCustomers(String branchName, boolean showTransactions) {
+    Branch branch = findBranch(branchName);
+    if (branch != null) {
+      System.out.println("The Customers for branch " + branch.getName() + "-");
+      ArrayList<Customer> branchCustomers = branch.getCustomers();
+      for (int i = 0; i < this.branches.size(); i++) {
+        Customer eachCustomer = branchCustomers.get(i);
+        System.out.println("Customer: " + eachCustomer.getName());
+        if (showTransactions) {
+          ArrayList<Double> transactions = eachCustomer.getTransactions();
+          System.out.println("Transactions: ");
+          for (Double j : transactions) {
+            System.out.println("$" + j);
+          }
+        }
+      }
+      return true;
+    }
+    return false;
   }
 }
 
